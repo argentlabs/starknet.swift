c922d68d7575e29160455302b23ba49a9d4666fc
Dionysios Karatzas <diokaratzas@gmail.com>
2025-09-23 22:53:43 +0300

    Removed the framework-style include guards
    from Sources/CFrameworkWrapper/include/bridge.h; we now import pedersen_hash.h, ecdsa.h, and poseidon.h directly, matching the new static xcframework layout.

--- Sources/CFrameworkWrapper/include/bridge.h
+++ Sources/CFrameworkWrapper/include/bridge.h
@@ -3,19 +3,10 @@
 
 #ifdef __OBJC__
 
-#if __has_include(<libcrypto_c_exports/pedersen_hash.h>)
-#import <libcrypto_c_exports/pedersen_hash.h>
-#import <libcrypto_c_exports/ecdsa.h>
-#else
 #import "pedersen_hash.h"
 #import "ecdsa.h"
-#endif
 
-#if __has_include(<libposeidon/poseidon.h>)
-#import <libposeidon/poseidon.h>
-#else
 #import "poseidon.h"
-#endif
 
 #import "crypto-rs.h"
 
